a2bc15eb74f10e3c064a1e03aa8f9698da19de08
CHARLES <charlescallaghan@gmail.com>
2019-01-25 16:35:46 +0100

    Added API request and async

--- src/reducers/albums.js
+++ src/reducers/albums.js
@@ -1,10 +1,13 @@
 import { ADD_ALBUM } from '../actions/AddAlbum'
+import { SET_ALBUMS } from '../actions/setAlbums'
+
 
 export default (state = [], action = {}) => {
     switch (action.type) {
         case ADD_ALBUM:
-            const albuminfo = action.payload
-            return [...state, albuminfo]
+            return [...state, action.payload]
+        case SET_ALBUMS:
+            return [...action.payload]
         default:
             return state
     }
